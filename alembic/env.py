86dec05c8990827a1f81fb9d9aa2b3f409312dfa
Proxypepe <sasha_golovin_95@bk.ru>
2022-12-01 20:23:33 +0300

    rename package name

--- alembic/env.py
+++ alembic/env.py
@@ -4,9 +4,9 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from app.core.settings import settings
-from app.db.database import Base
-from app.models.user import User
+from src.core.settings import settings
+from src.db.database import Base
+from src.models.user import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
